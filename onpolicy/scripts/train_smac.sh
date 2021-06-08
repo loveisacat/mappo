89dd11a862f3617891f7665bfce04c23c3ec34b3
Bin Chen <bchen7@ictdls-01.cis.utas.edu.au>
2021-06-08 19:41:55 +1000

    add my own wandb user_name

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 env="StarCraft2"
-map="corridor"
+#map="corridor"
+map="4m"
 algo="mappo"
 exp="mlp"
 seed_max=1
@@ -9,5 +10,5 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is 
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} --n_training_threads 127 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 5 --use_value_active_masks --use_eval --use_recurrent_policy
+    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} --n_training_threads 127 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 --num_env_steps 200000 --ppo_epoch 5 --use_value_active_masks --use_eval --use_recurrent_policy --user_name "loveisacat"
 done
